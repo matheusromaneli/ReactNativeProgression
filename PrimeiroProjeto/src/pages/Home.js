d5a31711f72474598b336c5c1d30b389463bc2b6
Matheus Romaneli <matheusromaneli@id.uff.br>
2021-09-16 16:59:00 -0300

    utilizando useState e props

--- PrimeiroProjeto/src/pages/Home.js
+++ PrimeiroProjeto/src/pages/Home.js
@@ -7,6 +7,13 @@ import {
 } from 'react-native';
 
 export function Home(){
+  const [newInput, setInput] = useState('')
+  const [historyInput, setNewInput] = useState([])
+
+  function handleAddNewInput(){
+    setNewInput(before => [...before, newInput]);
+  }
+
   return(
     <SafeAreaView style= {styles.container}>
 
@@ -15,19 +22,32 @@ export function Home(){
       <TextInput 
         style = {styles.input}
         placeholder = 'Digite algo'
-        placeholderTextColor = '#fefefe'  
+        placeholderTextColor = '#fefefe'
+        onChangeText = {setInput} 
       />
 
       <TouchableOpacity 
         style = {styles.button}
         activeOpacity = {0.5}
+        onPress = {handleAddNewInput}
       >
         <Text style = {styles.button_text}>Buscar</Text>
       </TouchableOpacity>
 
       <Text styles = {[styles.title, {marginTop: 20}]}>
-        Histórico de digitação
+        Histórico de digitação:
       </Text>
+
+      {
+        historyInput.map( input => (
+          <TouchableOpacity key={input} styles = {styles.historyList}>
+            <Text styles = {styles.button_text}>
+              {input}
+            </Text>
+          </TouchableOpacity>
+        ))
+      }
+
     </SafeAreaView>
   )
 }
@@ -58,5 +78,13 @@ const styles = StyleSheet.create({
   button_text:{
     color: '#fafafa',
     fontSize: 15
+  },
+  historyList:{
+    color: '#fafafa',
+    backgroundColor: '#a3a3a3',
+    padding: 10,
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginTop: 5
   }
 })
